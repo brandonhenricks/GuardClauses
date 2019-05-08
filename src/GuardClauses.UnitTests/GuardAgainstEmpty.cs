b727b38dc5fcdabe7f2a5791dc68cbc701226b77
Brandon Henricks <brandon.henricks1984@gmail.com>
2019-05-08 19:20:21 -0400

    Empty String Unit Test Added

--- src/GuardClauses.UnitTests/GuardAgainstEmpty.cs
+++ src/GuardClauses.UnitTests/GuardAgainstEmpty.cs
@@ -10,12 +10,14 @@ namespace GuardClauses.UnitTests
 		public void ThrowsGivenFoo()
 		{
 			Assert.Throws<ArgumentException>(() => Guard.Against.Empty(Guid.Empty, "aGuid"));
+			Assert.Throws<ArgumentException>(() => Guard.Against.Empty(String.Empty, "aString"));
 		}
 
 		[Fact]
 		public void DoesNothingGivenAnythingElse()
 		{
 			Guard.Against.Empty(Guid.NewGuid(), "aGuid");
+			Guard.Against.Empty("test", "aString");
 		}
 	}
 }
